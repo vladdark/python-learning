171f7a1f99650d225b6a3041a85044a4863dc0c1
Rodolfo Vladimir Carmona Noguera <vladcarmona6@gmail.com>
2020-11-01 21:10:28 -0600

    Using keyword arguments

--- functions/app.py
+++ functions/app.py
@@ -1,8 +1,8 @@
-def greet_user(name):
-    print(f"Hi {name}!")
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}!")
     print("Welcome aboard!")
 
 
 print("Start")
-greet_user('John')
+greet_user(last_name="Smith", first_name='John')
 print("Finish")
