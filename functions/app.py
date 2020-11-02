4f829ab9feb095edd500b408d3c1bd718651d1e9
Rodolfo Vladimir Carmona Noguera <vladcarmona6@gmail.com>
2020-11-01 21:04:59 -0600

    Using parameter in function

--- functions/app.py
+++ functions/app.py
@@ -1,8 +1,8 @@
-def greet_user():
-    print("Hi there!")
+def greet_user(name):
+    print(f"Hi {name}!")
     print("Welcome aboard!")
 
 
 print("Start")
-greet_user()
+greet_user('John')
 print("Finish")
