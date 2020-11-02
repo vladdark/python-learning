b1421e26f0ed86279370f9f184deb641f88b43c4
Rodolfo Vladimir Carmona Noguera <vladcarmona6@gmail.com>
2020-11-01 23:44:42 -0600

    creating ecommerce module importing and working with ecommerce module

--- modules/app.py
+++ modules/app.py
@@ -12,4 +12,10 @@ print(converters.kg_to_lbs(70))
 # ------------------------------------ USING UTILS FUNCTIONS
 numbers = [3, 6, 2, 8, 4, 10]
 print(f'array of numbers: {numbers}')
+print(f'Max number is: {max(numbers)}')
 print(f'Max number is: {find_max(numbers)}')
+print(f'Max number is: {utils.find_max(numbers)}')
+
+# ------------------------------------ USING ECOMMERCE MODULE
+import ecommerce.shipping
+ecommerce.shipping.calc_shipping()
---
+++ modules/ecommerce/shipping.py
@@ -0,0 +1,2 @@
+def calc_shipping():
+    print('executing calc_shipping')
