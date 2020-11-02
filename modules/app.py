fed2392f131740535db1f33f0e7a755a5322077c
Rodolfo Vladimir Carmona Noguera <vladcarmona6@gmail.com>
2020-11-02 13:40:51 -0600

    added functions to utils.py file to show comments and titles in the console using pathlib module and Path class to get information about files and directories in a path

--- modules/app.py
+++ modules/app.py
@@ -1,22 +1,24 @@
 import converters
 import utils
 from converters import kg_to_lbs
-from utils import find_max
+from utils import find_max, cout_brk, cout_title, cout_comment
 
-# ------------------------------------ USING CONVERTERS MODULE
+cout_title('USING CONVERTERS MODULE')
+
 print(kg_to_lbs(100))
 print(kg_to_lbs(70))
 
 print(converters.kg_to_lbs(70))
 
-# ------------------------------------ USING UTILS FUNCTIONS
+cout_title('USING UTILS FUNCTIONS')
+
 numbers = [3, 6, 2, 8, 4, 10]
 print(f'array of numbers: {numbers}')
 print(f'Max number is: {max(numbers)}')
 print(f'Max number is: {find_max(numbers)}')
 print(f'Max number is: {utils.find_max(numbers)}')
 
-# ------------------------------------ USING ECOMMERCE MODULE
+cout_title('USING ECOMMERCE MODULE')
 # import ecommerce
 # from ecommerce import shipping
 from ecommerce.shipping import calc_shipping
@@ -28,26 +30,25 @@ calc_shipping()
 calc_shipping()
 calc_shipping()
 
-# ------------------------------------ GENERATING RANDOM NUMBERS
+cout_title('GENERATING RANDOM NUMBERS')
 import random
 
-print('\n generating random numbers (float)')
+cout_comment('generating random numbers (float)')
 for i in range(3):
     print(random.random())
 
-print('\n generating random numbers (int)')
+cout_comment('generating random numbers (int)')
 for i in range(3):
     print(random.randint(10, 20))
 
-print('\n get random names from array')
+cout_comment('get random names from array')
 members = ['John', 'Mary', 'Bob', 'Mosh']
 for i in range(3):
     print(random.choice(members))
 
+cout_title('EXERCISE: ROLLING DICES')
 
-# ------------------------------------ EXERCISE: ROLLING DICES
-print('\nEXERCISE: ROLING DICES')
-print('\nMy Solution')
+cout_comment('My Solution')
 
 
 class MyDice:
@@ -56,9 +57,9 @@ class MyDice:
 
 
 dice = MyDice()
-print(f'\nResult of rolling dices: {dice.roll()}')
+print(f'Result of rolling dices: {dice.roll()}')
 
-print('\nMosh Solution')
+cout_comment('Mosh Solution')
 
 
 class MoshDice:
@@ -71,3 +72,37 @@ class MoshDice:
 dice = MoshDice()
 print(dice.roll())
 
+cout_title('PYTHON MODULES: PATH')
+from pathlib import Path
+
+# Absolute path
+# windows: c:\Program Files\Microsoft
+# mac: /usr/local/bin
+# Relative path
+cout_comment("'ecommerce' path exist")
+path = Path("ecommerce")
+print(path.exists())
+
+cout_comment("'ecommerce1' path doesn't exist")
+path = Path("ecommerce1")
+print(path.exists())
+
+cout_comment("creating and deleteing 'emails' folder")
+path = Path("emails")
+print(path.exists())
+# path.mkdir()  # uncomment if you want to create the directory
+# path.rmdir()  # uncomment if you want to remove the directory
+print(path.exists())
+
+cout_comment("finding all files and directories in a path")
+path = Path()
+print(path.glob('*.*'))  # all files
+print(path.glob('*.py'))  # all python files
+
+cout_comment('listing all python files')
+for file in path.glob('*.py'):
+    print(file)
+
+cout_comment('listing all files and directories')
+for element in path.glob('*'):
+    print(element)
--- modules/utils.py
+++ modules/utils.py
@@ -4,3 +4,18 @@ def find_max(numbers):
         if number > maximum:
             maximum = number
     return maximum
+
+
+def cout_brk(breaks=2):
+    print('\n' * breaks)
+
+
+def cout_title(title, breaks=2):
+    title = str(title).upper()
+    cout_brk(breaks)
+    print(f'# ------------------------- {title}')
+
+
+def cout_comment(comment, breaks=0):
+    cout_brk(breaks)
+    print(f'## {comment}')
