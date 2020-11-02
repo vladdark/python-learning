6c32265df19467bafa7da4bc49c013e4b94ec31e
Rodolfo Vladimir Carmona Noguera <vladcarmona6@gmail.com>
2020-11-01 21:15:58 -0600

    practicing with positional and keyword arguments

--- functions/app.py
+++ functions/app.py
@@ -3,6 +3,11 @@ def greet_user(first_name, last_name):
     print("Welcome aboard!")
 
 
+def calc_cost(total, shipping, discount):
+    print(f'total: {total}, shipping: {shipping}, discount: {discount}')
+
+
 print("Start")
-greet_user(last_name="Smith", first_name='John')
+greet_user('John', "Smith")
+calc_cost(total=50, shipping=5, discount=0.1)
 print("Finish")
