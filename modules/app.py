4ba2b81e2b65c8391dac5ff0d656a92497b388d4
Rodolfo Vladimir Carmona Noguera <vladcarmona6@gmail.com>
2020-11-01 23:32:55 -0600

    creating utils module importing it in the app file importing individual function

---
+++ modules/app.py
@@ -0,0 +1,15 @@
+import converters
+import utils
+from converters import kg_to_lbs
+from utils import find_max
+
+# ------------------------------------ USING CONVERTERS MODULE
+print(kg_to_lbs(100))
+print(kg_to_lbs(70))
+
+print(converters.kg_to_lbs(70))
+
+# ------------------------------------ USING UTILS FUNCTIONS
+numbers = [3, 6, 2, 8, 4, 10]
+print(f'array of numbers: {numbers}')
+print(f'Max number is: {find_max(numbers)}')
